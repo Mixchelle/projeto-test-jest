364dec3fef0b8482e57370284c30edffcd095cf4
Mixchelle <michelle.biologia@hotmail.com>
2024-08-21 22:27:07 -0300

    casos de teste para a função productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,43 +1,34 @@
 const productDetails = require('../src/productDetails');
-/*
-  A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara')
-
-  // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  Escreva pelo menos cinco testes para essa função para garantir que a implementação de productDetails está correta.
-
-*/
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('deve verificar se productDetails é uma função', () => {
+    expect(typeof productDetails).toBe('function');
+  });
+
+  it('deve retornar um array', () => {
+    const result = productDetails('Alcool gel', 'Máscara');
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  it('deve retornar um array contendo dois itens', () => {
+    const result = productDetails('Alcool gel', 'Máscara');
+    expect(result.length).toBe(2);
+  });
+
+  it('deve garantir que cada item no array retornado é um objeto', () => {
+    const result = productDetails('Alcool gel', 'Máscara');
+    expect(typeof result[0]).toBe('object');
+    expect(typeof result[1]).toBe('object');
+  });
+
+  it('deve garantir que os dois objetos no array são diferentes', () => {
+    const result = productDetails('Alcool gel', 'Máscara');
+    expect(result[0]).not.toEqual(result[1]);
+  });
+
+  it('deve garantir que os productIds terminam com "123"', () => {
+    const result = productDetails('Alcool gel', 'Máscara');
+    expect(result[0].details.productId.endsWith('123')).toBe(true);
+    expect(result[1].details.productId.endsWith('123')).toBe(true);
   });
 });
